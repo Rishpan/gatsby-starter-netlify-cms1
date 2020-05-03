e5ac7e9fbe84d043238831352a765f68d54ca284
Rishpan <34177557+Rishpan@users.noreply.github.com>
2020-05-03 11:48:19 -0700

    migrated home page

---
+++ src/templates/template.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from "gatsby"
+
+export default (props) => (
+    <div>
+        <header class="rounded-corners">
+            <div class="header-top">
+                <h1>Coinnit</h1>
+                <button>Add a Coin</button>
+            </div>
+            <div id="search">
+                <input type="text" placeholder="Search for a coin" />
+            </div>
+        </header>
+        <nav class = "rounded-corners">
+            <Link to="/">Home</Link>
+            <Link to="/news">News</Link>
+            <Link to="/about">About</Link>
+        </nav>
+        {props.children}
+    </div>
+);
