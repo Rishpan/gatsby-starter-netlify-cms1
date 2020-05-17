7a6f5aceea688d9abb2841740de4005fecaae5b9
Rishpan <34177557+Rishpan@users.noreply.github.com>
2020-05-17 11:11:35 -0700

    template moved, config edited

---
+++ src/pages/coininfo/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Template from '../pages/template'
+import imgurl from '../penny.jpg';
+
+export default function CoinInfo() {
+  const { coin } = useRouteData()
+  console.log(coin);
+  return (
+    <Template>
+      <div>
+        <h1>{coin.Title}</h1>
+        <div class="information"></div>
+        <table>
+          <tr>
+            <td>Value</td>
+            <td>{coin.Value}</td>
+          </tr>
+          <tr>
+            <td>Country</td>
+            <td>{coin.Country}</td>
+          </tr>
+          <tr>
+            <td>History</td>
+            <td>{coin.History}</td>
+          </tr>
+          <tr>
+            <td>Composition</td>
+            <td>{coin.Composition}</td>
+          </tr>
+        </table>
+        <img class="fit-picture"
+              src={imgurl}
+              alt="Penny picture">
+        </img>
+        </div>
+    </Template>
+  )
+}
