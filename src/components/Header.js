d86a52d9989435b24e4bf92621860942c0d27ed7
Ryan Cheung <cheung31@gmail.com>
2020-06-21 14:41:16 -0700

    Add search to header

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StaticQuery, graphql } from "gatsby"
+import Search from './search';
+
+export default function Header() {
+    return (
+        <StaticQuery
+    query={graphql`
+      query SearchIndexQuery {
+        siteSearchIndex {
+          index
+        }
+      }
+    `}
+    render={data => (
+        <header class="rounded-corners">
+            <div class="header-top">
+                <h1>Coinnit</h1>
+                <button>Add a Coin</button>
+            </div>
+            <div id="search">
+                <Search searchIndex={data.siteSearchIndex.index} />
+            </div>
+        </header>
+    )}
+  />
+        
+    )
+}
--- src/pages/template.js
+++ src/pages/template.js
@@ -1,18 +1,11 @@
 import React from 'react'
 import { Link } from "gatsby"
+import Header from "../components/Header";
 import '../components/all.sass'
 
 export default (props) => (
     <div>
-        <header class="rounded-corners">
-            <div class="header-top">
-                <h1>Coinnit</h1>
-                <button>Add a Coin</button>
-            </div>
-            <div id="search">
-                <input type="text" placeholder="Search for a coin" />
-            </div>
-        </header>
+        <Header />
         <nav class = "rounded-corners">
             <Link to="/">Home</Link>
             <Link to="/news">News</Link>
