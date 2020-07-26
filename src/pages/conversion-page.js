4c33c004ad2211a94b828f38ba9a69a050725ba6
Rishpan <34177557+Rishpan@users.noreply.github.com>
2020-07-26 12:40:49 -0700

    Added conversion page

---
+++ src/pages/conversion-page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Template from './template';
+
+export default function Conversion(){
+    return (
+        <Template>
+            <p>test</p>
+            <footer><center>Designed using React and Gatsby</center></footer>
+            <footer><center>Created by Rishab Pangal under the guidance of Ryan Cheung</center></footer>
+            <footer><center>If you have any questions, contact me at rishab.pangal@gmail.com</center></footer>
+        </Template>
+    );
+}
